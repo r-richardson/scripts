82972ad72e003dc67d76e08fae9ed7ae2c76cad6
r-richardson <robert.richardson@suse.com>
2023-10-29 12:43:22 +0100

    Refactorize and simplify dns override script

--- bash/dns_override.sh
+++ bash/dns_override.sh
@@ -6,26 +6,14 @@ add_entry() {
     domain=$1
     ip=$2
     echo "$ip $domain" >> "$hosts_file"  # Append the entry to the host file
+    echo "Added entry: $ip $domain"
 }
 
 remove_entry() {
     domain=$1
     sed -i "/$domain/d" "$hosts_file"  # Remove the line containing the specified domain from the host file
+    echo "Removed entry: $domain"
 }
-
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 
-    exit 1
-fi
-
-if [[ $# -lt 3 ]]; then
-    echo "Usage: $0 <add/remove> <domain> <ip>"
-    exit 1
-fi
-
-action=$1
-domain=$2
-ip=$3
 
 # Check if the script is run as root
 if [[ $EUID -ne 0 ]]; then
@@ -34,23 +22,17 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Check the number of command-line arguments
-if [[ $# -lt 3 ]]; then
-    echo "Usage: $0 <add/remove> <domain> <ip>"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <domain> <ip>"
     exit 1
 fi
 
-case $action in
-    "add")
-        add_entry "$domain" "$ip"
-        echo "Added entry: $ip $domain"
-        ;;
-    "remove")
-        remove_entry "$domain"
-        echo "Removed entry: $domain"
-        ;;
-    *)
-        echo "Invalid action. Use 'add' or 'remove'"
-        exit 1
-        ;;
-esac
+domain=$1
+ip=$2
 
+# Check if domain is already in the hosts file
+if grep -q -E "(^|\s)${domain}($|\s)" "$hosts_file"; then
+    remove_entry "$domain"
+else
+    add_entry "$domain" "$ip"
+fi
